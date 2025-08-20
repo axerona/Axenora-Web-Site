1c38d67736fe5894afa6c45ff597067dcc0e5a15
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2025-08-20 12:06:04 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,14 +4,15 @@ const Navbar = ({ activeTab, setActiveTab }) => {
   const links = ["Home", "Services", "Pricing", "Careers", "Contact"];
 
   return (
-    <nav className="flex justify-between items-center px-8 py-6 text-white absolute w-full top-0 z-10">
-      
+    <nav className="flex justify-end items-center px-8 py-6 text-white absolute w-full top-0 z-10">
       <ul className="flex space-x-8 text-sm font-medium">
         {links.map((link) => (
           <li
             key={link}
             className={`cursor-pointer hover:text-indigo-400 ${
-              activeTab.toLowerCase() === link.toLowerCase() ? "text-indigo-400 font-semibold" : ""
+              activeTab.toLowerCase() === link.toLowerCase()
+                ? "text-indigo-400 font-semibold"
+                : ""
             }`}
             onClick={() => setActiveTab(link.toLowerCase())}
           >
